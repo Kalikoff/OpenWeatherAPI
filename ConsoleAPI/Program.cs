18a290fb8cf4c0c1674fc4982c948b949a9f3207
Kalikoff <kalikoff.s@yandex.ru>
2023-03-01 19:56:06 +0300

    First commit

---
+++ ConsoleAPI/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleAPI {
+    class Program {
+        public static string CountryID;
+
+        static void Main(string[] args) {
+            Console.WriteLine("Введите ID города: ");
+            CountryID = Console.ReadLine(); // Москва - 524901
+
+            try {
+                ConnectAsync().Wait();
+                Console.WriteLine("Успешно");
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+                Console.Write("Город не найден или что-то пошло не так");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static async Task ConnectAsync() {
+            WebRequest request = WebRequest.Create(
+                "https://api.openweathermap.org/data/2.5/weather?id=" +
+                CountryID +
+                "&units=metric&APPID=??????");
+            request.Method = "POST";
+            WebResponse response = await request.GetResponseAsync();
+            string answer = "";
+
+            using (Stream s = response.GetResponseStream()) {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream())) {
+                    answer = await reader.ReadToEndAsync();
+                }
+            }
+
+            response.Close();
+
+            WeatherResponse response_global = JsonConvert.DeserializeObject<WeatherResponse>(answer);
+            Console.WriteLine("Средняя температура в данный момент в городе " + response_global.name + " = " + response_global.main.temp);
+            Console.WriteLine("Погода за окном: " + response_global.weather[0].main + " Описание: " + response_global.weather[0].description);
+        }
+    }
+}
---
+++ ConsoleAPI/Weather.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleAPI {
+    public class Temperatura {
+        public double temp;
+    }
+
+    public class WeatherNow {
+        public string main;
+        public string description;
+    }
+
+    public class WeatherResponse {
+        public Temperatura main;
+        public string name;
+        public WeatherNow[] weather;
+    }
+}
